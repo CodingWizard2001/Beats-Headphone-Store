ce6ad5d0f3f30065dc35553fcbc8c4861821522a
Rahul Goswami <155483433+CodingWizard2001@users.noreply.github.com>
2024-03-17 04:59:59 +0530

    Update script.js

--- Headphone Store/script.js
+++ Headphone Store/script.js
@@ -17,3 +17,18 @@ window.onscroll = () =>{
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+// Scroll Reveal Animation
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+sr.reveal('.home-text',{origin:'left'})
+sr.reveal('.home-img',{origin:'right'})
+sr.reveal('.heading',{delay:200})
+sr.reveal('.specs-details .box',{origin:'left',interval: 200})
+sr.reveal('.specs-img',{delay:600})
+sr.reveal('.shop-container .box','.footer .logo','.footer .footer-box',{interval: 150})
